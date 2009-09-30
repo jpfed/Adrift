ff821820d5d440071ff85c857309de541dffc461
jpfed <jerry.federspiel@gmail.com>
2009-09-30 00:19:11 -0500

    grabbed and tweaked portal graphics

--- love/objects/goodies/WarpPortal.lua
+++ love/objects/goodies/WarpPortal.lua
@@ -5,7 +5,12 @@ love.filesystem.require("graphics/RepresentableAsImage.lua")
 WarpPortal = {
   super = SimplePhysicsObject,
   sound = love.audio.newSound("sound/portal.ogg"),
-
+  
+  hazeIntense = love.graphics.newColor(255,200,255,200),
+  hazeFade = love.graphics.newColor(64,20,128,64),
+  darkIntense = love.graphics.newColor(24,0,32,200),
+  darkFade = love.graphics.newColor(0,0,0,64),
+  
   draw = function(self)
     local x, y, scale = camera:xy(self.x, self.y, 0)
     love.graphics.setColorMode(love.color_modulate)
@@ -22,9 +27,17 @@ WarpPortal = {
       love.graphics.draw(self.darkmatter,x,y)
     end
     love.graphics.setColorMode(love.color_normal)
+    love.graphics.draw(self.image,x,y,scale/25)
   end,
   
   update = function(self, dt)
+    if state.game.ship.hasCrystal then
+      self.darkmatter:setParticleLife(0.375,1.0)
+      self.haze:setParticleLife(0.5,1.5)
+    else
+      self.darkmatter:setParticleLife(0.25,0.5)
+      self.haze:setParticleLife(0.25,0.5)
+    end
     self.haze:update(dt)
     self.darkmatter:update(dt)
   end,
@@ -41,7 +54,7 @@ WarpPortal = {
     result.haze = love.graphics.newParticleSystem(love.graphics.newImage("graphics/smoke.png"), 300)
     local h = result.haze
     h:setEmissionRate(40)
-    h:setParticleLife(2.5,3.0)
+    h:setParticleLife(0.5,1.5)
     h:setDirection(0)
     h:setRotation(0,360)
     h:setSpin(0,360,1.0)
@@ -51,14 +64,14 @@ WarpPortal = {
     h:setTangentialAcceleration(80,200)
     h:setGravity(0)
     h:setSize(3.0,12.0,0.8)
-    h:setColor(love.graphics.newColor(255,200,255,200), love.graphics.newColor(64,20,128,64))
+    h:setColor(result.hazeIntense, result.hazeFade)
     h:start()
     h:update(2)
 
     result.darkmatter = love.graphics.newParticleSystem(love.graphics.newImage("graphics/smoke.png"), 300)
     local d = result.darkmatter
     d:setEmissionRate(40)
-    d:setParticleLife(1.5,2.0)
+    d:setParticleLife(0.375,1.0)
     d:setDirection(0)
     d:setRotation(0,360)
     d:setSpin(0,360,1.0)
@@ -68,10 +81,12 @@ WarpPortal = {
     d:setTangentialAcceleration(40,60)
     d:setGravity(0)
     d:setSize(4.0,8.0,1.0)
-    d:setColor(love.graphics.newColor(24,0,32,200), love.graphics.newColor(0,0,0,64))
+    d:setColor(result.darkIntense, result.darkFade)
     d:start()
     d:update(2)
 
+    result.image = love.graphics.newImage("graphics/WarpPortal.png")
+    
     return result
   end
 }

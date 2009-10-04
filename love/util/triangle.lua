20607374adc493fcb3b02c4cb582d271a0cc1c36
Dan Fitch <dan@rapt.us>
2009-10-03 23:53:28 -0500

    some more triangle tests and an inch of actual functionality

--- love/util/triangle.lua
+++ love/util/triangle.lua
@@ -1,6 +1,26 @@
+love.filesystem.require("util/geom.lua")
 
 Triangle = {
   create = function(self,p1,p2,p3,p4,p5,p6)
     return {p1,p2,p3,p4,p5,p6}
+  end,
+
+  lengths = function(t)
+    local l1 = geom.distance(t[1], t[2], t[3], t[4])
+    local l2 = geom.distance(t[3], t[4], t[5], t[6])
+    local l3 = geom.distance(t[5], t[6], t[1], t[2])
+    return {l1,l2,l3}
+  end,
+
+  area = function(t)
+    return 3
+  end,
+
+  has_overlap = function(t1, t2)
+    return false
+  end,
+
+  has_inside = function(t1, t2)
+    return false
   end
 }
--- test/test_Triangle.lua
+++ test/test_Triangle.lua
@@ -8,9 +8,53 @@ function test_MakesTriangles()
   assertTableEquals( t, {0,0,3,0,3,4} )
 end
 
-function test_TrianglesNotOverlapping()
+function test_LengthsSimple()
+  assertTableEquals( Triangle.lengths({0,0,3,0,3,4}), {3,4,5} )
+end
+
+function test_LengthsComplex()
+  local lengths = Triangle.lengths({0,0,3,3,0,4})
+  assertEqual( lengths[1], math.sqrt(18) )
+end
+
+function test_Area()
+  assertTrue( Triangle.area({0,0,3,0,3,4}), 6 )
+end
+
+function test_NotOverlapping()
   local t1 = Triangle:create(0,0,3,0,3,4)
   local t2 = Triangle:create(100,0,13,0,13,4)
+  assertFalse( Triangle.has_overlap(t1, t2) )
+end
+
+function test_SharingEdge()
+  local t1 = Triangle:create(0,0,3,0,3,4)
+  local t2 = Triangle:create(0,0,3,4,0,4)
+  assertFalse( Triangle.has_overlap(t1, t2) )
+end
+
+function test_OverlappingByContains()
+  local t1 = Triangle:create(0,0,3,0,3,4)
+  local t2 = Triangle:create(-10,-1,10,-1,10,10)
+  assertTrue( Triangle.has_overlap(t1, t2) )
+end
+
+function test_Inside()
+  local t1 = Triangle:create(-10,-1,10,-1,10,10)
+  local t2 = Triangle:create(0,0,3,0,3,4)
+  assertTrue( Triangle.has_inside(t1, t2) )
+end
+
+function test_InsideSame()
+  local t2 = Triangle:create(0,0,3,0,3,4)
+  local t2 = Triangle:create(0,0,3,0,3,4)
+  assertTrue( Triangle.has_inside(t1, t2) )
+end
+
+function test_NotInside()
+  local t1 = Triangle:create(0,0,2,0,2,1)
+  local t2 = Triangle:create(0,0,3,0,3,4)
+  assertFalse( Triangle.has_inside(t1, t2) )
 end
 
 runTests { useANSI = false }

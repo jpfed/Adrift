cc5fafef3168ec945bb59503e2e54e5f2864f6db
jpfed <jerry.federspiel@gmail.com>
2009-10-04 20:36:51 -0500

    mouseless menus

--- love/states/menu.lua
+++ love/states/menu.lua
@@ -9,44 +9,43 @@ getMenu = function(opts, extras)
     yMargin = 13,
     
     cursor = {
-      x = 400,
-      y = 300,
-      selected = 0,
+      
+      selected = 1,
+      cooldown = 0,
       
       draw = function(c,s) 
-        if c.selected > 0 then
-          love.graphics.setColor(s.highlightColor)
-          c.rep = "x"
-          local so = s.options[c.selected]
-          love.graphics.rectangle(love.draw_line,so.x - s.xMargin,so.y - s.yMargin,so.w,so.h)
-        else
-          love.graphics.setColor(s.normalColor)
-          c.rep = "+"
-        end
-        love.graphics.draw(c.rep,c.x,c.y)
+        love.graphics.setColor(s.highlightColor)
+        local so = s.options[c.selected]
+        love.graphics.rectangle(love.draw_line,so.x - s.xMargin,so.y - s.yMargin,so.w,so.h)
       end,
-      
-      update = function(c) 
-        c.x = love.mouse.getX()
-        c.y = love.mouse.getY()
-      end
     },
 
     options = opts,
     supplemental = extras,
     
-    update = function(s,dt) 
-      s.cursor:update()
-      local found = false
-      for k,v in ipairs(s.options) do
-        if s.cursor.x > v.x - s.xMargin and s.cursor.x < v.x + v.w - s.xMargin then
-          if s.cursor.y > v.y - s.yMargin and s.cursor.y < v.y + v.h - s.yMargin then
-            s.cursor.selected = k
-            found = true
-          end
+    update = function(s,dt)
+      local x,y = love.joystick.getAxes(0)
+      local gamepad = love.joystick.getHat(0,0)
+      local c = s.cursor
+      
+      local up = gamepad == love.joystick_hat_up or y < -0.25 or love.keyboard.isDown(love.key_up)
+      local down = gamepad == love.joystick_hat_down or y > 0.25 or love.keyboard.isDown(love.key_down)
+      local left = gamepad == love.joystick_hat_left or x < -0.25 or love.keyboard.isDown(love.key_left)
+      local right = gamepad == love.joystick_hat_right or x > 0.25 or love.keyboard.isDown(love.key_right)
+      
+      local select = love.keyboard.isDown(love.key_return) 
+      
+      if select then s.options[c.selected].action(); return end
+      
+      if c.cooldown == 0 then
+        if up then c.selected = s.options[c.selected].up; c.cooldown = c.cooldown + 0.125
+        elseif down then c.selected = s.options[c.selected].down; c.cooldown = c.cooldown + 0.125
+        elseif left then c.selected = s.options[c.selected].left; c.cooldown = c.cooldown + 0.125
+        elseif right then c.selected = s.options[c.selected].right; c.cooldown = c.cooldown + 0.125
         end
+      else
+        s.cursor.cooldown = math.max(0,s.cursor.cooldown - dt)
       end
-      if not found then s.cursor.selected = 0 end
     end,
     
     draw = function(s) 
@@ -63,9 +62,12 @@ getMenu = function(opts, extras)
     end,
     
     keypressed = function(s,key) 
-      if key ~= love.key_return then return end
-      if s.cursor.selected > 0 then s.options[s.cursor.selected].action() end
+    end,
+    
+    joystickpressed = function(s,j,b)
+      s.options[s.cursor.selected].action()
     end
+    
   }
 end
 
@@ -74,22 +76,30 @@ local options = {
   {text = "Start Game", x = 480, y = 200, w = 95, h = 20,
     action = function()  
       state.game:load()
-  end},
+  end,
+  up = 4, down = 2, left = 1, right = 1,
+  },
   
   {text = "Options", x = 480, y = 300, w = 75, h = 20,
     action = function()  
       state.current = state.options
-  end},
+  end,
+  up = 1, down = 3, left = 2, right = 2,
+  },
   
   {text = "Help", x = 480, y = 350, w = 60, h = 20,
     action = function()
       state.current = state.help
-  end},
+  end,
+  up = 2, down = 4, left = 3, right = 3,
+  },
   
   {text = "Quit", x = 480, y = 400, w = 60, h = 20,
     action = function() 
       love.system.exit()
-  end}
+  end,
+  up = 3, down = 1, left = 4, right = 4,
+  }
 }
 
 local supplemental = {

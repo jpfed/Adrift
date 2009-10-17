01fda7cfb7f34d601b3b6c13cdd9fbda755ab82f
Dan Fitch <dan@rapt.us>
2009-10-17 17:46:24 -0500

    Jointedness doesn't work

--- love/objects/enemies/Leech.lua
+++ love/objects/enemies/Leech.lua
@@ -26,58 +26,27 @@ Leech = {
     mixin(r, Leech)
     r.class = Leech
 
-    local leechPoints = {{x=3,y=0}, {x=2,y=1}, {x=-2,y=1}, {x=-3,y=0}, {x=-2,y=-1}, {x=2,y=-1}}
+    local leechPoints = {{x=6,y=0}, {x=5,y=1}, {x=1,y=0.8}, {x=0,y=0}, {x=1,y=-0.8}, {x=5,y=-1}}
     r.part1 = r:addConvexBlob(
       { damping = 0.1, adamping = 0.1 },
       { scale = 0.1, points = leechPoints, color = self.color, color_edge = self.color_edge } )
     r.part2 = r:addConvexBlob(
       { damping = 0.1, adamping = 0.1 },
-      { scale = 0.1, points = leechPoints, color = self.color, color_edge = self.color_edge, offset={x=4,y=0} } )
+      { scale = 0.1, points = leechPoints, color = self.color, color_edge = self.color_edge, offset={x=5,y=0} } )
     r.part3 = r:addConvexBlob(
       { damping = 0.1, adamping = 0.1 },
-      { scale = 0.1, points = leechPoints, color = self.color, color_edge = self.color_edge, offset={x=7,y=0} } )
+      { scale = 0.1, points = leechPoints, color = self.color, color_edge = self.color_edge, offset={x=10,y=0} } )
+
     -- Now to joint them together!
+    love.physics.newRevoluteJoint( r.part1.body, r.part2.body, 0.55, 0 ) 
+    love.physics.newRevoluteJoint( r.part2.body, r.part3.body, 1.1, 0 ) 
 
 
     mixin(r, DamageableObject:prepareAttribute(difficulty,nil,Leech.deathSound,1000))
     
-    r.engine = Engine:create(r, Leech.thrust, 2, 8)
-    r.thruster = FireThruster:create(r, 180)
-    
     r.collisionReaction = math.random()*90-45
     r.coolRate = math.random()+0.5
     return r
-  end,
-  
-  update = function(self, dt)
-    self.superUpdate(self, dt)
-    
-    -- go for the main character unless you are recovering from a collision
-    local forceX, forceY = 0, 0
-    if self.collisionShock == 0 then
-      local tx, ty = state.game.ship.x, state.game.ship.y
-      local dx, dy = tx - self.x, ty - self.y
-      local anorm = math.max(0.01,math.sqrt(dx*dx + dy*dy))
-      if anorm < 36 then 
-        forceX, forceY = dx / anorm, dy / anorm
-      else
-        forceX, forceY = math.random()*2-1, math.random()*2-1.1
-      end
-    else
-      -- back away from the wall and move along it in a consistent direction
-      local targTheta = math.rad(self.angle + self.collisionReaction)
-      forceX, forceY = - math.cos(targTheta), - math.sin(targTheta)
-      self.collisionShock = math.max(0,self.collisionShock - dt)
-    end
-    
-    local overallThrust = self.engine:vector(forceX, forceY, dt)
-    self.thruster:setIntensity(overallThrust*5)
-    self.thruster:update(dt)
-  end,
-  
-  draw = function(self)
-    self.thruster:draw()
-    self:superDraw()
   end,
   
   cleanup = function(self)

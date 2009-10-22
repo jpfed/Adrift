24f98179ac05d837370eee1e5f093d494bbdac38
jpfed <jerry.federspiel@gmail.com>
2009-10-21 23:39:22 -0500

    REPL supports shift key, robust compilation

--- love/states/repl.lua
+++ love/states/repl.lua
@@ -10,14 +10,49 @@ state.repl = {
 
   keypressed = function(s,key) 
     if key == love.key_return then
-      local func = assert(loadstring("return " .. s.input))
-      logger:add(tostring(func()))
+      local validFunc, errorMessage = loadstring("return " .. s.input)
+      if validFunc then
+        logger:add(tostring(validFunc()))
+      else
+        logger:add("Compilation failed: " .. errorMessage)
+      end
       s.input = ""
     elseif key == love.key_backspace then
       s.input = s.input:sub(1, s.input:len() - 1)
     elseif key < 200 then
-      s.input = s.input .. string.char(key)
+      local toAdd = string.char(key)
+      if love.keyboard.isDown(love.key_lshift) or love.keyboard.isDown(love.key_rshift) then
+        toAdd = s.shiftMap[toAdd]
+      end
+      s.input = s.input .. toAdd
     end
   end
 }
 
+state.repl.shiftMap = {}
+local sm = state.repl.shiftMap
+
+sm["1"] = "!"
+sm["2"] = "@"
+sm["3"] = "#"
+sm["4"] = "$"
+sm["5"] = "%"
+sm["6"] = "^"
+sm["7"] = "&"
+sm["8"] = "*"
+sm["9"] = "("
+sm["0"] = ")"
+sm["-"] = "_"
+sm["="] = "+"
+sm["\\"] = "|"
+sm["["] = "{"
+sm["]"] = "}"
+sm[";"] = ":"
+sm["'"] = "\""
+sm[","] = "<"
+sm["."] = ">"
+sm["/"] = "?"
+-- translate small letter + shift into capital letter
+for asciiCode = 97,122 do
+  sm[string.char(asciiCode)] = string.char(asciiCode - 32)
+end

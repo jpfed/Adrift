b97069321fdb791ca5b4aff38def362a8baf1fbf
jpfed <jerry.federspiel@gmail.com>
2009-10-23 08:09:37 -0500

    New music

--- love/sound/sound.lua
+++ love/sound/sound.lua
@@ -1,5 +1,9 @@
 sound = {
-  bgm = love.audio.newMusic("sound/theme.ogg"),
+  bgmIndex = 1,
+  bgm = {
+    love.audio.newMusic("sound/metroidish.ogg"),
+    love.audio.newMusic("sound/theme.ogg"),
+  },
   weapon = {
     love.audio.newSound("sound/littleshot.ogg")
   }

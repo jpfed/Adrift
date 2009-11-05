719f591d4ac8b41f640e92d81f9781ee304ed3a3
jpfed <jerry.federspiel@gmail.com>
2009-11-04 23:25:20 -0600

    removing texturing; ensuring convexity

--- love/objects/goodies/MineralChunk.lua
+++ love/objects/goodies/MineralChunk.lua
@@ -21,35 +21,13 @@ MineralChunk = {
   create = function(self,point)
     local r = MultipleBlobObject:create(point.x,point.y)
 
-    mixin(r, CollectibleObject:attribute(self.sound, self.effect))
+    --mixin(r, CollectibleObject:attribute(self.sound, self.effect))
     mixin(r, MineralChunk)
     r.class = MineralChunk
 
-    local rand  = function() return math.random(15) / 10.0 end
-    -- I'm sure there's a more idiomatic way to do this kind of perturbation, 
-    -- but I'm lazy right now! WANTY RESULTS!
-    local p = {
-      n  = function(p) return {x=p.x,y=p.y-rand()} end,
-      s  = function(p) return {x=p.x,y=p.y+rand()} end,
-      e  = function(p) return {x=p.x+rand(),y=p.y} end,
-      w  = function(p) return {x=p.x-rand(),y=p.y} end,
-      ne = function(p) local r = rand(); return {x=p.x+r,y=p.y-r} end,
-      nw = function(p) local r = rand(); return {x=p.x-r,y=p.y-r} end,
-      sw = function(p) local r = rand(); return {x=p.x-r,y=p.y+r} end,
-      se = function(p) local r = rand(); return {x=p.x+r,y=p.y+r} end
-    }
-    local p1 = {
-      p.ne({x=1,y=-1}),
-      p.nw({x=-1,y=-1}),
-      p.sw({x=-1,y=1}),
-      p.se({x=1,y=1})
-    }
-    local p2 = {
-      p.n({x=0,y=-1.5}),
-      p.w({x=-1.5,y=0}),
-      p.s({x=0,y=1.5}),
-      p.e({x=1.5,y=0})
-    }
+    local p1 = self:generateRandomConvex()
+    local p2 = self:generateRandomConvex()
+    
     r.scale = math.random(1,4) / 20.0
     r.blob = r:addConvexBlob(
       { damping = 0.1, adamping = 0.1 },
@@ -57,36 +35,28 @@ MineralChunk = {
     r.blob:addConvexShape(
       { scale = r.scale, points = p2, color = self.color, color_edge = self.color_edge } )
 
-
-
-    r.texture = love.graphics.newParticleSystem(love.graphics.newImage("graphics/smoke.png"), 100)
-    local h = r.texture
-    h:setEmissionRate(3)
-    h:setParticleLife(r.scale * 40,r.scale * 80)
-    h:setDirection(0)
-    h:setRotation(0,360)
-    h:setSpread(360)
-    h:setSpeed(0.1,0.2)
-    h:setRadialAcceleration(0,0.5)
-    h:setGravity(0)
-    h:setSize(r.scale * 2,r.scale * 2)
-    h:setColor(self.color_texture1, self.color_texture2)
-    h:start()
-    h:update(120)
-
     return r
   end,
 
   update = function(self,dt)
     MultipleBlobObject.update(self,dt)
-    self.texture:update(dt)
   end,
 
   draw = function(self)
     MultipleBlobObject.draw(self)
-    local x,y,scale = L:xy(self.x,self.y,0)
-    love.graphics.setColorMode(love.color_modulate)
-    love.graphics.draw(self.texture,x,y)
-    love.graphics.setColorMode(love.color_normal)
-  end
+  end,
+  
+  generateRandomConvex = function(self)
+    local result = {}
+    local angles = {}
+    local numPoints = math.random(3,8)
+    for k = 1, numPoints do
+      table.insert(angles, math.random()*2*math.pi)
+    end
+    table.sort(angles)
+    for k = 1, numPoints do
+      table.insert(result, {x = 1.5*math.cos(angles[k]), y = 1.5*math.sin(angles[k])})
+    end
+    return result
+  end,
 }
